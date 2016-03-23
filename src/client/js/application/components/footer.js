fb00d11313eb99b1f5c36de57a2c3d419926d4b9
Matti andreas nielsen <mattinielsen5@hotmail.com>
2016-03-24 00:47:23 +0100

    herpderp

--- src/client/js/application/components/footer.js
+++ src/client/js/application/components/footer.js
@@ -11,7 +11,7 @@ export default class Footer extends React.Component {
     render() {
         return(
             <div>
-                    Footer haha
+                    Footer haha !
             </div>
         );
     }
