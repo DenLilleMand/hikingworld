ebf7c4c1a143b0130f7f76dde5c8e8ee04d30869
Matti andreas nielsen <mattinielsen5@hotmail.com>
2016-03-24 02:36:23 +0100

    automated commit for CI

--- ci.sh
+++ ci.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git add .
 git commit -m "automated commit for CI"
-git pull origin master
+git pull origin master -X theirs
 forever stopall
 webpack
 forever start ./src/server/index.js
