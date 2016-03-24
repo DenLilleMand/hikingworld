d31818ca2a70fb2ea605ec7d299f9b6b73ef5036
Matti andreas nielsen <mattinielsen5@hotmail.com>
2016-03-24 02:16:13 +0100

    automated commit for CI

--- src/server/controllers/api/ci/ci.js
+++ src/server/controllers/api/ci/ci.js
@@ -1,9 +1,16 @@
 var exports = module.exports;
+var fileSystem = require('fs');
 
 const execFile = require('child_process').execFile;
 
 exports.build = (request, response, callback) => {
-    execFile('./ci.sh');
+    execFile('./ci.sh', (error, stdout, stderr) => {
+        fileSystem.writeFile('ci_log.txt', error, (err) => {
+            if(err) {
+                console.log(err);
+            }
+        });
+    });
     console.log('herpderp!');
     callback();
 };
