c336c00d2c1123ffa12eecc6a78f48e8038ce1d5
Matti andreas nielsen <mattinielsen5@hotmail.com>
2016-03-24 11:49:28 +0100

    automated commit for CI

--- ci.sh
+++ ci.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 git add .  >> gitadd.txt
 git commit -m "automated commit for CI"  >> logs/gitcommit.txt
-git pull origin master -X theirs >> git logs/pull.txt
+git pull origin master -X theirs >> logs/pull.txt
 forever stopall >> logs/foreverstopall.txt
 webpack >> logs/webpack.txt
 forever start ./src/server/index.js >> logs/foreverstart.txt
