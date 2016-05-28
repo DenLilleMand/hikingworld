ef08f11f4d776aca73d0376b1500dccbc5b43c1b
Whitechapel <victoremil.r.andersen@gmail.com>
2016-05-28 21:26:38 +0200

    ads

--- src/server/model/models/account.js
+++ src/server/model/models/account.js
@@ -19,6 +19,7 @@ module.exports = function (sequelize, DataTypes) {
             field: 'password',
             allowNull: false
         },
+        
         salt: {
             type: DataTypes.STRING(145),
             field: 'salt'
