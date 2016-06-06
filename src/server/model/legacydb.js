261238c392375f87a67b92cdda4b65694d1d3111
Whitechapel <victoremil.r.andersen@gmail.com>
2016-06-06 12:16:29 +0200

    A new user - appuser with specific privilegies

--- src/server/model/legacydb.js
+++ src/server/model/legacydb.js
@@ -2,11 +2,11 @@ var config = require('../config/configuration/configuration.json');
 var mysql  = require('mysql');
 
 var pool = mysql.createPool({
-    connectionLimit: config.database.connectionLimit,
-    host: config.database.host,
-    user: config.database.user,
-    password: config.database.password,
-    database: config.database.database
+    connectionLimit: config.database_appuser.connectionLimit,
+    host: config.database_appuser.host,
+    user: config.database_appuser.user,
+    password: config.database_appuser.password,
+    database: config.database_appuser.database
 });
 
 module.exports = {
